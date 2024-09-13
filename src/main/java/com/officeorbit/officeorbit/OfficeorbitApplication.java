11e9bb6a06c74d4674b6b36760e395130440bd74
Hubert <hubert.gawczynski@gmail.com>
2024-09-13 23:22:36 +0200

    skeleton

---
+++ src/main/java/com/officeorbit/officeorbit/OfficeorbitApplication.java
@@ -0,0 +1,13 @@
+package com.officeorbit.officeorbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficeorbitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfficeorbitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/officeorbit/officeorbit/OfficeorbitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.officeorbit.officeorbit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfficeorbitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
